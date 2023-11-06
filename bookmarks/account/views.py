acf504fe74b964cf630cc859a36445364395b3a2
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-05 21:14:29 -0300

    User registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -11,9 +11,7 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(
-                request, username=cd["username"], password=cd["password"]
-            )
+            user = authenticate(request, username=cd["username"], password=cd["password"])
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -30,3 +28,19 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Save the User object
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
